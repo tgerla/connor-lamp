9c70579b08acd8cbeecca0daafd23954f8affd2f
Timothy Gerla <tim@gerla.net>
2020-10-20 19:14:20 -0400

    pattern upgrades

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ OneButton myButton(buttonPin1, PULLUP);
 #define NUM_LEDS 16
 CRGBArray<NUM_LEDS> leds;
 
-int currentEffect = 3, setupEffect = -1;
+int currentEffect = 5, setupEffect = -1;
 
 typedef void (*EffectFunction)(int);
 
@@ -83,7 +83,7 @@ void eyes(int clicker) {
     fadeRate = 0;
     darkCounter = 0;
 
-    fill_solid(leds, NUM_LEDS, CHSV(0, 0, 0));
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
     setupEffect = currentEffect;
   }
 
@@ -101,10 +101,6 @@ void eyes(int clicker) {
     //    fill_rainbow(&(leds[eyesLit]), 1 /*led count*/, random8());
     eyesHue = random8();
     leds[eyesLit].setHSV(eyesHue, 255, 255);
-    Serial.println(eyesHue);
-    Serial.println(eyesLit);
-
-    FastLED.show();
   } else if (eyesCounter == 0 && darkCounter > 0) { // waiting for dark counter
     darkCounter--;
   } else if (eyesCounter > 0 && darkCounter > 0) { // dimming eyes
@@ -137,19 +133,43 @@ void nightlight(int clicker) {
   drawTwinkles(leds, 1, nightlightPal, CRGB(8, 0, 0), 1);
 }
 
-DEFINE_GRADIENT_PALETTE(Pink_Purple_gp){
-    0,   19,  2,   39,  25,  26,  4,   45,  51,  33,  6,   52,  76,  68,  62,
-    125, 102, 118, 187, 240, 109, 163, 215, 247, 114, 217, 244, 255, 122, 159,
-    149, 221, 149, 113, 78,  188, 183, 128, 57,  155, 255, 146, 40,  123};
-CRGBPalette16 purplesPal = Pink_Purple_gp;
+// clang-format off
+DEFINE_GRADIENT_PALETTE(Teal_gp){
+      0, 0,   255, 255,
+    128,   0, 255, 255,
+    255,   0,   0,   0,
+};
+DEFINE_GRADIENT_PALETTE(HotPink_gp){
+      0, 255,   0, 166,
+    128, 255,   0, 166,
+    255,   0,   0,   0,
+};
+
+DEFINE_GRADIENT_PALETTE(TealAndPink_gp){
+      0,   0,   255, 255,
+    128,   0,   255, 255,
+    129, 255,   0,   166,
+    255, 255,   0,   166,
+};
+
+CRGBPalette16 tealPal = Teal_gp, pinkPal = HotPink_gp, tealAndPinkPal = TealAndPink_gp;
+CRGBPalette16 currentPal;
+// clang-format on
 
 void purples(int clicker) {
   if (setupEffect != currentEffect) {
     // set up nightlight
     FastLED.setBrightness(255);
     setupEffect = currentEffect;
+    currentPal = tealAndPinkPal;
   }
-  drawTwinkles(leds, 2, purplesPal, CRGB::MediumPurple, 5);
+  /*  if (clicker < 128 )
+      nblendPaletteTowardPalette(currentPal, tealPal, 24);
+    else
+      nblendPaletteTowardPalette(currentPal, pinkPal, 24);
+  */
+
+  drawTwinkles(leds, 2, currentPal, CRGB::Black, 5);
 }
 
 ///////////////////////////////// MAIN LIGHT CODE ///////////////////////////
@@ -159,7 +179,7 @@ void lightUpdate() {
   effectFunctions[currentEffect](idx);
 
   FastLED.show();
-  FastLED.delay(1000 / FRAMES_PER_SECOND);
+  //  FastLED.delay(1000 / FRAMES_PER_SECOND);
 
   ledClock++;
   if (ledClock > CYCLE_SPEED) {
@@ -205,6 +225,8 @@ void setup() {
 }
 
 void loop() {
-  myButton.tick();
-  lightUpdate();
+  EVERY_N_MILLISECONDS(1000 / FRAMES_PER_SECOND) {
+    myButton.tick();
+    lightUpdate();
+  }
 }
